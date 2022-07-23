5fe3a5f1143f4e1acb2d4ba928407fa295d063b6
Dayane <dayanecarolina.oliveira@gmail.com>
2022-07-23 16:44:55 -0300

    initialized default files

---
+++ index.js
@@ -0,0 +1,20 @@
+import express, { json } from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import db from "./db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Funcionando!");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(chalk.bold.green(`Servidor conectado na porta ${PORT}.`));
+});
